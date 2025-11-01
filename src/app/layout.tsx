80a83c234c11a7fef2b86c97b3c72f47bb164b4a
petrioteer <petrioteer007@gmail.com>
2025-11-01 16:41:29 +0530

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Itesh's Portfolio",
+  description: "Portfolio website of Itesh Tomar",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-light text-secondary">
+      <h1 className="text-4xl font-semibold">Hello, I’m Itesh 👋</h1>
+    </main>
+  );
+}
